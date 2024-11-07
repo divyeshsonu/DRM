652f4d3f8acaea2bddd6fe373c6fed96a331e070
divyeshsonu <44664822+divyeshsonu@users.noreply.github.com>
2024-11-07 08:41:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Config(object):
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
-    LOG_CH = os.environ.get("LOG_CH", "-2320155220")
+    LOG_CH = os.environ.get("LOG_CH", "1291491834")
 
 # TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
 logging.basicConfig(
